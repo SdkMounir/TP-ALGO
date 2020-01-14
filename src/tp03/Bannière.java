fd7967a860a1a5d25bf4d3a7608bb5430d1c7398
SOIDIKI MOUNIR <mounirsoidiki@gmail.com>
2020-01-14 16:50:03 +0100

    Dernier Tp Algo

---
+++ src/tp03/Bannière.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp03;
+
+/**
+ *
+ * @author formation_gep
+ */
+public class Bannière {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        afficheBannière();
+        // Suite programme
+    }
+    public static void afficheBannière() {
+        System.out.println("********************************************");
+        System.out.println("**                                        **");
+        System.out.println("**               BIENVENUE                **");
+        System.out.println("**                                        **");
+        System.out.println("********************************************");
+        
+        
+    }
+}
